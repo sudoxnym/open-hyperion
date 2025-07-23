cc35c8d98c5fbddfbf4a847c847b39de42bd81fa
Adam Honse <calcprogrammer1@gmail.com>
2025-07-22 20:42:55 -0500

    Add Qt6 Linux builds

--- scripts/build-plugin.sh
+++ scripts/build-plugin.sh
@@ -8,11 +8,26 @@ set -x
 set -e
 
 #-----------------------------------------------------------------------#
+# Setup environment                                                     #
+#-----------------------------------------------------------------------#
+if [ "$1" = "qt6" ]; then
+    export QT_SELECT=qt6
+else
+    export QT_SELECT=qt5
+fi
+
+if [ "$QT_SELECT" = "qt6" ]; then
+    export QMAKE=qmake6
+else
+    export QMAKE=qmake
+fi
+
+#-----------------------------------------------------------------------#
 # Configure build files with qmake                                      #
 # we need to explicitly set the install prefix, as qmake's default is   #
 # /usr/local for some reason...                                         #
 #-----------------------------------------------------------------------#
-qmake .
+$QMAKE .
 
 #-----------------------------------------------------------------------#
 # Build project and install files into AppDir                           #
